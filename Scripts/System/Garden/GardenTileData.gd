c0e4ef4d0724580787386b6f40cf085cd22b48c6
quichez <57053917+quichez@users.noreply.github.com>
2024-02-21 20:27:06 -0500

    GardenTileData created
    This will need to be merged in later

---
+++ Scripts/System/Garden/GardenTileData.gd
@@ -0,0 +1,24 @@
+class_name GardenTileData
+
+var age : float
+
+#Soil Nutrients - NPK
+var nitrogen : float
+var phosphorus : float
+var potassium : float
+
+#Soil Nutrients - Major
+var magnesium : float
+var calcium : float
+var sulfur : float
+
+#Soil Nutrients - Trace
+var copper : float
+var manganese : float
+var iron : float
+var molybdenum : float
+var zinc : float
+var chlorine : float
+var boron : float
+
+
